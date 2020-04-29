bec86d08d9b160bb7339d96aeb849f7856fc80b1
Shimeng <36157060+ShimengZhao@users.noreply.github.com>
2020-04-29 22:16:12 +0200

    correct dependency; change version number

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name="sharepoint-on-prem",
-    version="0.0.0",
+    version="0.0.1",
     author="Shimeng Zhao",
     author_email="zhaoshimeng2015@gmail.com",
     packages=["sp_on_prem"],
-    install_requires=["requests_ntlm"],
+    install_requires=["requests_ntlm2"],
 )
